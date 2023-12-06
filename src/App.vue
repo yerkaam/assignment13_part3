af3e636d834ebe1f08f0431d5b7f52721c289620
yerkaam <eraorynbai@gmail.com>
2023-12-06 22:54:18 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,61 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="main">
+  <carousel :itemsToShow="1.5">
+    <slide v-for="(list, index) in lists" :key="index">
+      <img :src="require(`/Users/eraorynbaigmail.com/Desktop/assignment13/part3/src/assets/${list.photo}`)" alt="Slide" />
+    </slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
+</div>
 </template>
 
+<script>
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import 'vue3-carousel/dist/carousel.css';
+
+
+
+export default {
+  name: 'App',
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+  data() {
+    return {
+      lists: [
+        { photo: 'ron6.png' },
+        { photo: 'ron7.png' },
+        { photo: 'ron8.png' },
+        { photo: 'ron9.png' },
+        { photo: 'ron10.png' },
+      ],
+    };
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  margin: 0;
+  padding: 0;
+}
+.main{
+ padding:120px 100px;
+ background-color: rgb(38, 40, 165);
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+img {
+  width: 300px;
+  height: 300px;
+  border-radius:10%;
+  outline: none;
+  border: none;
 }
 </style>
